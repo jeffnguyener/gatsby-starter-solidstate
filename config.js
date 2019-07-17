c212f838ed5c6a82c9bc85ab6848b5ce17d3f2a0
Jeff Nguyen <jeffnguyener@Samanthas-MacBook-Pro-2.local>
2019-07-16 23:05:05 -0600

    update config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteTitle: 'HJN Portfolio', // <title>
+  siteTitle: 'HJN', // <title>
   manifestName: 'Portfolio',
   manifestShortName: 'Landing', // max 12 characters
   manifestStartUrl: '/',
@@ -7,7 +7,7 @@ module.exports = {
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/gatsby-starter-solidstate/`, // This path is subpath of your hosting https://domain/portfolio
+  pathPrefix: `quizzical-shirley-342bcc.netlify.com`, // This path is subpath of your hosting https://domain/portfolio
   heading: 'Jeff Nguyen',
   subHeading: 'Web Developer',
   // social
