bc8ed8022022a3859c645c024f3a53be5e8d4e1b
jeffnguyener <jeffnguyen.83@gmail.com>
2023-01-02 09:27:40 -0600

    update skills

--- src/pages/index.js
+++ src/pages/index.js
@@ -44,7 +44,7 @@ const IndexPage = () => (
             <h2 className="major">Knowledge & Skills</h2>
             <p>
               HTML ∙ CSS3 ∙ JavaScript ∙ PostgreSQL ∙ SASS ∙ Adobe Creative Cloud ∙ 
-              Git ∙ Microsoft Office ∙ Visual Studio Code ∙ Adobe XD ∙ JIRA
+              Git ∙ Microsoft Office ∙ Visual Studio Code ∙ Adobe XD ∙ JIRA ∙ SharePoint
             </p>
             <a href="/Projects" className="special">
             My Portfolio
