4ac33421800686c8f2e7fee7e610bf178e61382f
hnguyen <hnguyen@health.southalabama.edu>
2023-02-09 13:34:25 -0600

    added new project

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -71,6 +71,22 @@ const IndexPage = () => (
                 Live site
               </a>
             </article>
+            <article>
+              <a href="https://xd.adobe.com/view/b0ef7c76-77d8-4b17-926e-715fda7649f8-e5bc/" className="image">
+                <img src={pic11} alt="usah1" />
+              </a>
+              <h3 className="major">USA Outlook Access Request</h3>
+              <p>
+                <b>Brief Project Description</b>
+                Currently, the USA Health Security Team manually manages which users in their system have access to shared mail inboxes and shared calendars as well as the level of permissions they are allowed. Typically, a user would reach out to an administrator and request access to a specific inbox or calendar, the administrator would then run the powershell commands that would give the user the necessary permissions and the level of access that they choose. The process proves to be very repetitive to the administrator. The project aims to automate this process by instead of  having the user reach out directly to the administrator, they would simply use a web application to request permissions. From this application, they would request select to request to either a mailbox or a calendar then if it is approved, a powershell command script within the application will run and provide the user the requested permissions.
+              </p>
+              <a
+                href="https://xd.adobe.com/view/b0ef7c76-77d8-4b17-926e-715fda7649f8-e5bc/"
+                className="special"
+              >
+                Adobe XD Prototype
+              </a>
+            </article>
            </section>
         </div>
       </div>
