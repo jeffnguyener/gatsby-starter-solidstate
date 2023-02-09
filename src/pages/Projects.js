0638de202af5f675c2697964603d7d4781878d79
hnguyen <hnguyen@health.southalabama.edu>
2023-02-09 13:57:20 -0600

    space

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -76,7 +76,7 @@ const IndexPage = () => (
               </a>
               <h3 className="major">USA Outlook Access Request</h3>
               <p>
-                <b>Brief Project Description</b>
+                <b>Brief Project Description</b><br></br>
                 Currently, the USA Health Security Team manually manages which users in their system have access to shared mail inboxes and shared calendars as well as the level of permissions they are allowed. Typically, a user would reach out to an administrator and request access to a specific inbox or calendar, the administrator would then run the powershell commands that would give the user the necessary permissions and the level of access that they choose. The process proves to be very repetitive to the administrator. The project aims to automate this process by instead of  having the user reach out directly to the administrator, they would simply use a web application to request permissions. From this application, they would request select to request to either a mailbox or a calendar then if it is approved, a powershell command script within the application will run and provide the user the requested permissions.
               </p>
               <a
